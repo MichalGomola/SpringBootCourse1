b71cb4cf1659a586d4f1889de7f0536702eca3d0
MichalGomola <michal.gomola.89@gmail.com>
2023-02-24 13:31:21 +0100

    thymeleaf loops lesson done

--- 02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ 02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -1,18 +1,24 @@
 package com.ltp.gradesubmission;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
-
 @Controller
 public class GradeController {
     
+    List<Grade> studentGrades = Arrays.asList(
+        new Grade("Harry", "Potions", "C-"),
+        new Grade("Hermione", "Arithmancy", "A+"), 
+        new Grade("Ron", "Charms", "D-"));
+    
     @GetMapping("/grades")
-    public String sayGrades(Model model) {
-        Grade grade = new Grade("Harry", "Potions", "C-");
-        model.addAttribute("grade", grade);
+    public String sayGrades(Model model) {      
+        model.addAttribute("grades", studentGrades);
         return "grades";
     }
 }
