7c33fa2fcd6525a9aceaac1ea981c5d59ee454ce
MichalGomola <michal.gomola.89@gmail.com>
2023-02-24 16:35:50 +0100

    form submission lesson done

--- 02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ 02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -1,24 +1,36 @@
 package com.ltp.gradesubmission;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class GradeController {
     
-    List<Grade> studentGrades = Arrays.asList(
-        new Grade("Harry", "Potions", "C-"),
-        new Grade("Hermione", "Arithmancy", "A+"), 
-        new Grade("Ron", "Charms", "D-"));
+    List<Grade> studentGrades = new ArrayList<>();
     
+    @GetMapping("/")
+    public String gradeForm(Model model) {
+        model.addAttribute("grade", new Grade());
+        return "form";
+    }
+
+    @PostMapping("/handleSubmit")
+    public String submitForm(Grade grade) {
+        studentGrades.add(grade);
+        return "redirect:/grades";
+
+    }
+
     @GetMapping("/grades")
     public String sayGrades(Model model) {      
         model.addAttribute("grades", studentGrades);
         return "grades";
     }
+
+    
 }
