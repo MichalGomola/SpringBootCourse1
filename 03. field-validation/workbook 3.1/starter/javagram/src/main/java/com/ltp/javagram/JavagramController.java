e36ba0d5c576eff0190fbd2bea2d3c20dadd23df
MichalGomola <michal.gomola.89@gmail.com>
2023-03-03 20:24:31 +0100

    workbook 3.1 done

--- 03. field-validation/workbook 3.1/starter/javagram/src/main/java/com/ltp/javagram/JavagramController.java
+++ 03. field-validation/workbook 3.1/starter/javagram/src/main/java/com/ltp/javagram/JavagramController.java
@@ -1,19 +1,30 @@
 package com.ltp.javagram;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class JavagramController {
     
     @GetMapping("/")
     public String getForm(Model model) {
+        model.addAttribute("user", new User());
         return "sign-up";
     }
 
     @GetMapping("/result")
     public String getResult() {
         return "result";
+    }
+
+    @PostMapping("/handleSubmit")
+    public String submitForm(@Valid User user, BindingResult result){
+        if (result.hasErrors()) return "sign-up";
+        return "redirect:/result";
     }
 }
