9dd2cd300aa1206bcde5f89b9f21b5f5fd5c2edf
MichalGomola <michal.gomola.89@gmail.com>
2023-03-11 13:04:14 +0100

    challenge 4 start

---
+++ 04. three-layer-codebase/global-superstore/starter/global-superstore/src/main/java/com/ltp/globalsuperstore/repository/ItemRepository.java
@@ -0,0 +1,12 @@
+package com.ltp.globalsuperstore.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ltp.globalsuperstore.Item;
+
+public class ItemRepository {
+
+    List<Item> items = new ArrayList<>();
+    
+}
